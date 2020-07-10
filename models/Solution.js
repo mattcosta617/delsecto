9788309556f01c231019ffb0652fecf804a66a6b
mattcosta617 <mattiecosta@gmail.com>
2020-07-10 17:55:11 -0400

    Home base

--- models/Solution.js
+++ models/Solution.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 // Database
-const db = require('.');
+const db = require('./', (req, res) => {
+    res.send("Hello! Welcome to Delsecto!")
+});
