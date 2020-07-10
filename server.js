437934b0202931f4ea2e99e49dc65919f427426a
mattcosta617 <mattiecosta@gmail.com>
2020-07-10 17:57:25 -0400

    submaster added

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 
+
+const db = require('./', (req, res) => {
+    res.send("Hello! Welcome to Delsecto!")
+});
+
+
 app.listen(PORT, () => console.log(``));
