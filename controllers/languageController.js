455a5c20ded5443f7550d42594b495c1a5474211
mattcosta617 <mattiecosta@gmail.com>
2020-07-10 18:34:03 -0400

    paths to controllers built

--- controllers/languageController.js
+++ controllers/languageController.js
@@ -1,9 +1,12 @@
+const mongoose = require('mongoose');
+
+
 const languageController = {
     language: {
         type: String,
     }
 };
 
-const Language = mongoose.model('LanguageController', LanguageController);
+const Language = mongoose.model('LanguageController', languageController);
 
 module.exports = Language;
--- server.js
+++ server.js
@@ -2,12 +2,40 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// --------------------------CALL CONTROLLERS-------------------------
+
+const languageCtrl = require('./controllers/languageController');
+const questionCtrl = require('./controllers/questionsController');
+const solutionsCtrl = require('./controllers/solutionsController');
+const userCtrl = require('./controllers/userController');
+
+// -------------------------VIEW ENGINE--------------------------------
+
 app.set('view engine', 'ejs');
 
+//---------------------------MIDDLEWARE--------------------------------
+
+
+// ----------------------------ROUTES---------------------------------------
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('home');
 });
 
+app.use('/languages', (req, res) => {
+    res.render(languageCtrl);
+});
+
+app.use('/questions', (req, res) => {
+    res.render(questionCtrl);
+});
+
+app.use('/solutions', (req, res) => {
+    res.render(solutionsCtrl);
+});
+
+app.use('/user', (req,res) => {
+    res.render(userCtrl);
+});
 
 app.listen(PORT, () => console.log(`The Server is running on port ${PORT}`));
