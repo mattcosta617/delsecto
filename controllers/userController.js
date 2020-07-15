b6a5ca2f8e231f4954dbbc56f1151462b83672be
ChaseTheCoder <chasefsheaff@gmail.com>
2020-07-15 17:54:51 -0500

    users up and working and login and stay in session

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,73 +1,120 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
+//DB
 const db = require('../models');
-const User = require('../models/User');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const LocalStrategy = require('passport-local');
-const passportLocalMongoose = require('passport-local-mongoose');
-const session = reqruire('express-session')
 
-router.use(bodyParser.urlencoded({extended:true}));
-
-router.get('/register', (req, res) => {
-    res.render('users/register');
+//Login
+router.get('/', (req, res) => {
+  res.render('users/login');
 });
-router.use(passport.initialize());
-router.use(passport.session());
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+//Register
+router.get('/register', (req, res) => {
+  res.render('users/register');
+});
 
 router.get('/show', (req, res) => {
     res.render('users/show');
 });
 
-router.post('/register', (req, res) => {
-    db.User.register(new db.User({
-        username:req.body.username
-    }),
-        req.body.password,(err, user) => {
-           if(err) {
-                console.log(err);
-                return res.render('users/register');
-            }
-            passport.authenticate('local')(req, res, function(){
-                res.redirect('users/show');
-           }); 
-        });
-    });
+router.post('/', (req, res) => {
+  db.User.findOne({username: req.body.username}, (err, foundUser) => {
+    if (err) return console.log(err);
 
-//login
-router.get('/', (req, res) => {
-    res.render('users/login');
-});
-
-router.post('/', passport.authenticate('local',{
-    successRedirect:'/users/show',
-    failureRedirect:'/users'
-}),function(req, res){
-    res.send("User is "+ req.user.id);
-});
-
-//logout
-router.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-});
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
+    if (!foundUser) {
+      return res.send('No User Found');
     }
-    res.redirect('users/login');
-}
 
-// User.find((err, foundUser) => {if (err)
-//     console.log(err); 
-//     console.log(foundUser); 
-//     process.exit();
+    bcrypt.compare(req.body.password, foundUser.password, (err, isMatch) => {
+      if (err) return console.log(err);
+
+      if (isMatch) {
+
+        const currentUser = {
+          _id: foundUser._id,
+          username: foundUser.username,
+          isLoggedIn: true,
+        }
+
+        req.session.currentUser = currentUser;
+        res.redirect('/users/show');
+      } else {
+
+        return res.send('Passwords do not match');
+      }
+    });
+  });
+});
+
+
+// Register Create
+router.post('/register', (req, res) => {
+  // Verify req.body Is Not Empty
+
+  // Query DB For Existing User By Email
+
+  // If foundUser, Respond with 400
+
+  // If No foundUser, Generate Salt and Hash User Password
+
+  // Replace newUser Plain Text Password with Hased Password
+
+  // Create newUser and Respond with 200
+
+  // Check For Existing User Account
+  db.User.findOne({username: req.body.username}, (err, foundUser) => {
+    if (err) return console.log(err);
+
+    // Return Error If Account Already Exists
+    if (foundUser) return console.log('User Already Exsists');
+
+    // Generate Hash Salt (This just makes the password hard to crack)
+    bcrypt.genSalt(10, (err, salt)=> {
+      if (err) return console.log(err);
+
+      // Turn the Plain Text Password Into A Complicated Hash
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) return console.log(err);
+
+        // Destructure New User Data From Request
+        const { username, password } = req.body;
+
+        // Construct New User Object with Hashed Password
+        const newUser = {
+          username,
+          password: hash, // VERY IMPORTANT! NEVER SAVE PLAIN TEXT PASSWORD
+        };
+
+        // Create New User
+        db.User.create(newUser, (err, createdUser) => {
+          if (err) return console.log(err);
+      
+          res.redirect('/users');
+        });
+      });
+    });
+  });
+});
+
+
+// Logout Route
+// router.get('/logout', (req, res) => {
+//   if (!req.session.currentUser) return res.send('You must be logged in to logout');
+
+//   req.session.destroy((err) => {
+//     if (err) return console.log(err);
+
+//     res.redirect('/users');
+//   });
 // });
+
+//Check Users That Have Been Created
+// db.User.find((err, foundUser) => {if (err)
+//     console.log(err); console.log(foundUser); process.exit();
+// });
+
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -4,11 +4,16 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        unique: true
+        required: true,
+        unique: true,
+        minlength: 2,
+        maxlength: 50,
     }, password: {
-        type: String 
+        type: String, 
+        required: true,
+        minlength: 4
     }
-});
+}, {timestamp: true});
 
 UserSchema.plugin(passportLocalMongoose);
 
