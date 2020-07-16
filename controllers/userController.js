6cc6d63e56090ad463cf5b4e805e87b66fbf1886
ChaseTheCoder <chasefsheaff@gmail.com>
2020-07-15 19:00:42 -0500

    cleaned up controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
-
-//DB
 const db = require('../models');
 
 //Login
@@ -16,7 +14,7 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-//////////////////////////////////////////////////
+//Profile home
 router.get('/show', (req, res) => {
     db.User.findById(req.params.id, (err, foundUser) => {
         if(err) return console.log(err);
@@ -59,43 +57,24 @@ router.post('/', (req, res) => {
 
 // Register Create
 router.post('/register', (req, res) => {
-  // Verify req.body Is Not Empty
-
-  // Query DB For Existing User By Email
-
-  // If foundUser, Respond with 400
-
-  // If No foundUser, Generate Salt and Hash User Password
-
-  // Replace newUser Plain Text Password with Hased Password
-
-  // Create newUser and Respond with 200
-
-  // Check For Existing User Account
   db.User.findOne({username: req.body.username}, (err, foundUser) => {
     if (err) return console.log(err);
 
-    // Return Error If Account Already Exists
     if (foundUser) return console.log('User Already Exsists');
 
-    // Generate Hash Salt (This just makes the password hard to crack)
     bcrypt.genSalt(10, (err, salt)=> {
       if (err) return console.log(err);
 
-      // Turn the Plain Text Password Into A Complicated Hash
       bcrypt.hash(req.body.password, salt, (err, hash) => {
         if (err) return console.log(err);
 
-        // Destructure New User Data From Request
         const { username, password } = req.body;
 
-        // Construct New User Object with Hashed Password
         const newUser = {
           username,
-          password: hash, // VERY IMPORTANT! NEVER SAVE PLAIN TEXT PASSWORD
+          password: hash, //hash = hide password
         };
 
-        // Create New User
         db.User.create(newUser, (err, createdUser) => {
           if (err) return console.log(err);
       
@@ -107,7 +86,7 @@ router.post('/register', (req, res) => {
 });
 
 
-//Logout Route
+//Logout
 router.get('/logout', (req, res) => {
   if (!req.session.currentUser) return res.send('You must be logged in to logout');
 
