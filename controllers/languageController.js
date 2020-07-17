c13ff7d78e482ac407183f5fbcf448dedecb04de
mattcosta617 <mattiecosta@gmail.com>
2020-07-17 13:51:54 -0400

    Languages back up and running

--- controllers/languageController.js
+++ controllers/languageController.js
@@ -2,17 +2,32 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// router.get('/', (req, res) => {
+
+//     db.Questions.findById(req.body.), (err, allQuestions) => {
+//         if(err) return console.log(err);
+//         db.User.findById(req.session.currentUser._id, (err, foundUser) => {
+//             if(err) return console.log(err);
+
+//             res.render('languages/index', {
+//                 questions: allQuestions,
+//                 user: foundUser,
+//             });
+//         });
+//     });
+// });
 
 router.get('/', (req, res) => {
 
-    db.Question.find({}, (err, allQuestions) => {
-        if (err) return console.log(err);
+    db.Language.find({}, (err, allLanguages) => {
+        if(err) return console.log(err);
         db.User.findById(req.session.currentUser._id, (err, foundUser) => {
             if(err) return console.log(err);
-        res.render('languages/index', {
-            questions: allQuestions,
-            user: foundUser,
-            });
+
+            res.render('languages/index', {
+                languages: allLanguages,
+                user: foundUser,
+            })
         });
     });
 });
