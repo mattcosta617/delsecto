cfc64a65c12db6d4b93ed0a29776979d27f661c7
ChaseTheCoder <chasefsheaff@gmail.com>
2020-07-17 10:11:02 -0500

    attempts to fix language pages without success

--- controllers/languageController.js
+++ controllers/languageController.js
@@ -2,17 +2,31 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// router.get('/', (req, res) => {
+
+//     db.Questions.findById(req.body.), (err, allQuestions) => {
+//         if(err) return console.log(err);
+//         db.User.findById(req.session.currentUser._id, (err, foundUser) => {
+//             if(err) return console.log(err);
+
+//             res.render('languages/index', {
+//                 questions: allQuestions,
+//                 user: foundUser,
+//             });
+//         });
+//     });
+// });
+
 router.get('/', (req, res) => {
 
-    db.Language.find({}, (err, allLanguages) => {
-        if(err) return console.log(err);
+    db.Question.find({}, (err, allQuestions) => {
+        if (err) return console.log(err);
         db.User.findById(req.session.currentUser._id, (err, foundUser) => {
             if(err) return console.log(err);
-
-            res.render('languages/index', {
-                languages: allLanguages,
-                user: foundUser,
-            })
+        res.render('languages/index', {
+            questions: allQuestions,
+            user: foundUser,
+            });
         });
     });
 });
